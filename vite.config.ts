6e85e2bcdfdf4f30fcca412872f496d9f0f66931
Dean Reymen <deenrayman@outlook.com>
2024-11-03 13:18:38 +0700

    chore: shadcn and button

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 
@@ -8,6 +9,11 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [tailwindcss()]
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
     }
   }
 });
