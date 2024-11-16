c4784aab1cd7efe0bf496d8723b9662ada51a131
Dean Reymen <deenrayman@outlook.com>
2024-11-16 19:12:08 +0100

    fix: mobile header

--- src/App.tsx
+++ src/App.tsx
@@ -7,32 +7,38 @@ function App() {
   const isPreview = location.pathname.includes('preview');
 
   return (
-    <div className="relative flex h-dvh flex-col overflow-hidden">
-      <header className="absolute left-0 right-0 top-6 flex w-full flex-row items-center justify-between gap-4 px-6">
-        <Link to={'/'} className="w-fit">
-          <Logo className="min-h-6 w-fit" />
-        </Link>
-        {isPreview ? (
-          <p className="text-sm text-muted-foreground">
-            This is a preview kanban board, go{' '}
-            <Link className="text-indigo-500 underline" to={'/'}>
-              back
-            </Link>{' '}
-            here
-          </p>
-        ) : (
-          <div></div>
-        )}
+    <div className="flex min-h-dvh flex-col bg-background">
+      <header className="fixed top-0 z-50 w-full border-b bg-background/50 backdrop-blur supports-[backdrop-filter]:bg-background/60 md:relative md:border-b-0">
+        <div className="flex h-16 items-center justify-between px-4 sm:px-6">
+          <Link to={'/'} className="w-fit">
+            <Logo className="h-6 min-h-6 w-[170px]" />
+          </Link>
+          {isPreview ? (
+            <p className="ml-4 text-sm text-muted-foreground">
+              This is a preview kanban board, go{' '}
+              <Link className="text-indigo-500 underline" to={'/'}>
+                back
+              </Link>{' '}
+              here
+            </p>
+          ) : (
+            <div></div>
+          )}
+        </div>
       </header>
-      <KanbanBoard className="flex-1 overflow-scroll px-6 pb-12 pt-[72px]" isPreview={isPreview} />
-      <footer className="absolute bottom-3 left-0 right-0 px-6">
-        <p className="text-sm text-muted-foreground">
-          ©2024 &#8212; Click{' '}
-          <Link className="text-indigo-500 underline" to={'/preview'}>
-            here
-          </Link>{' '}
-          to view a preview
-        </p>
+
+      <KanbanBoard className="h-full flex-1 px-4 pt-[80px] sm:px-6 md:pt-0" isPreview={isPreview} />
+
+      <footer className="w-full">
+        <div className="flex h-14 items-center px-4 sm:px-6">
+          <p className="text-sm text-muted-foreground">
+            ©2024 &#8212; Click{' '}
+            <Link className="text-indigo-500 underline" to={'/preview'}>
+              here
+            </Link>{' '}
+            to view a preview
+          </p>
+        </div>
       </footer>
     </div>
   );
